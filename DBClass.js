7a19359aa0d8b8234c6570ed335c2ae61e5f2e3b
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-03-02 15:51:43 +0200

    creating the class DataBase and more
    also:
    1. MakeNewShortenedUrl(url) method that accept given url and creating object with shortened url and more.
    2. creating the function dateToSqlFormat() that changing the date to sql format.
    3. creating the function shortUrlGenerator() that generate random 5 character string

---
+++ DBClass.js
@@ -0,0 +1,68 @@
+// Class for DataBase
+class DataBase {
+  constructor() {
+    this.urlsArr = [];
+  }
+
+  MakeNewShortenedUrl(url) {
+    const newUrlObject = {};
+    newUrlObject.creationDate = dateToSqlFormat();
+    newUrlObject.redirectCount = 0;
+    newUrlObject.originalUrl = url;
+    newUrlObject["shorturl-id"] = shortUrlGenerator();
+    this.urlsArr.push(newUrlObject);
+  }
+}
+
+  let timeCreation = new Date();
+  timeCreation =
+    timeCreation.toISOString().split("T")[0] +
+    " " +
+    timeCreation.toTimeString().split(" ")[0];
+  return timeCreation;
+}
+
+function shortUrlGenerator() {
+  const charArr = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+  let string = "";
+  for (let i = 0; i < 5; i++) {
+    string += charArr[Math.floor(Math.random() * 35)];
+  }
+  return string;
+}
