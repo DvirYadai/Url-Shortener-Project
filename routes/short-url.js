77968e9d5646346bd89505fdaf72fe47d94c0a98
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-03-03 19:03:22 +0200

    adding route to short-url.js
    adding route that redirect when entering http://localhost:3000/api/shorturl/:shorturlid to the real url and updating the url redirect count

--- routes/short-url.js
+++ routes/short-url.js
@@ -6,4 +6,16 @@ router.get("/", (req, res) => {
   res.send(dataBase.MakeNewShortenedUrl("https://www.google.com"));
 });
 
+router.get("/:shorturlid", (req, res) => {
+  const { shorturlid } = req.params;
+  const { originalUrl } = dataBase.GetSpecificUrl(shorturlid);
+  try {
+    dataBase.updateUrlredirectCount(shorturlid);
+    res.redirect(302, originalUrl);
+  } catch (error) {
+    console.log(error);
+    res.send();
+  }
+});
+
 module.exports = router;
