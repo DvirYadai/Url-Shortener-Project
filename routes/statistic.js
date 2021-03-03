a7399e3db49b8f6e74fcbcc75d8a437701598842
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-03-03 19:04:19 +0200

    adding route to statistic.js
    adding route that give me statistic to a specific shortened url.

--- routes/statistic.js
+++ routes/statistic.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const dataBase = require("../DBClass");
 
-router.get("/:shorturl-id", (req, res) => {
+router.get("/", (req, res) => {
   res.send("this is a api/statistic/:shorturl-id route");
+});
+
+router.get(`/:shorturlid`, (req, res) => {
+  const { shorturlid } = req.params;
+  res.send(dataBase.GetSpecificUrl(shorturlid));
 });
 
 module.exports = router;
