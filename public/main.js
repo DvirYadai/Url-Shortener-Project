816e258c18dd6c15e8b5c2310982c00d07c6cc74
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-03-04 16:01:59 +0200

    adding event listener and axios
    adding event listener to the statistic button and axios get to /api/statistic/ route

--- public/main.js
+++ public/main.js
@@ -1,9 +1,19 @@
 document
-  .getElementById("submit-button")
+  .getElementById("url-submit-button")
   .addEventListener("click", async (e) => {
     e.preventDefault();
-    const inputValue = document.getElementById("url_input").value;
+    let inputValue = document.getElementById("url_input").value;
     await postNewUrl(inputValue);
+    document.getElementById("url_input").value = "";
+  });
+
+document
+  .getElementById("shortened-submit-button")
+  .addEventListener("click", async (e) => {
+    e.preventDefault();
+    let inputValue = document.getElementById("shortened_url_input").value;
+    await GetSpecificUrlStatistic(inputValue);
+    document.getElementById("url_input").value = "";
   });
 
 async function postNewUrl(url) {
@@ -19,6 +29,18 @@ async function postNewUrl(url) {
       console.log(res.data[0]);
       return;
     }
+    console.log(res.data);
+  } catch (error) {
+    console.log(error.response.data);
+  }
+}
+
+async function GetSpecificUrlStatistic(url) {
+  try {
+    const res = await axios({
+      method: "GET",
+      url: `http://localhost:3000/api/statistic/${url}`,
+    });
     console.log(res.data);
   } catch (error) {
     console.log(error.response.data);
