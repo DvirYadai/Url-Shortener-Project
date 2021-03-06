6afd18a89c835e60bad6b1e33650ecbdd2724ed0
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-03-06 20:48:09 +0200

    adding notes to url-validation.js

--- net-utils/url-validation.js
+++ net-utils/url-validation.js
@@ -1,12 +1,14 @@
 const isUrl = require("is-valid-http-url");
 const fetch = require("node-fetch");
 
+// Function that checks if a given short url is in format or not.
 function isShortenedUrlInFormat(url) {
   if (!/^[a-z0-9]{5}$/.test(url)) {
     throw "Invalid shortened url format!";
   }
 }
 
+// Function that check if given url is valid or not.
 async function isUrlValid(url) {
   if (!isUrl(url)) {
     throw "Invalid Url!";
@@ -17,6 +19,7 @@ async function isUrlValid(url) {
   }
 }
 
+// Function that check if given url exist or not.
 async function isUrlReal(url) {
   return await fetch(url)
     .then((res) => {
