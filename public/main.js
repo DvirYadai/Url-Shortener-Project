dbf6222e4d913f5e50f758ab451be8d4b5860d45
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-03-06 16:25:02 +0200

    updating the main.js (open description)
    1) adding error handling that show to the user whats the error.
    2) adding the short url to the html when the user make new short url.
    3)adding statistic div when the user give a short url id.

--- public/main.js
+++ public/main.js
@@ -1,15 +1,31 @@
+const errorUrl = document.getElementById("error-url");
+const errorShortUrl = document.getElementById("error-shortened-url");
+const errorNewShortUrl = document.getElementById("error-new-shortened-url");
+const newShortUrlP = document.getElementById("new-short-url");
+const shortUrlAnchor = document.getElementById("short-url-anchor");
+const statisticDiv = document.getElementById("statistic");
+const creationTimeP = document.getElementById("creation-time");
+const originalUrlP = document.getElementById("original-url");
+const redirectCountP = document.getElementById("redirect-count");
+const shorturlIdP = document.getElementById("shorturl-id");
+
 document
   .getElementById("url-submit-button")
   .addEventListener("click", async (e) => {
     e.preventDefault();
     let inputValue = document.getElementById("url_input").value;
+    if (inputValue === "") {
+      errorUrl.hidden = false;
+      errorUrl.innerText = "You must enter url!";
+      return;
+    }
     let inputCustomShortUrlValue = document.getElementById(
       "shortened_url_input_option"
     ).value;
+    newShortUrlP.hidden = true;
+    errorUrl.hidden = true;
+    errorNewShortUrl.hidden = true;
     await postNewUrl(inputValue, inputCustomShortUrlValue);
-    document.getElementById("url_input").value = "";
-    document.getElementById("shortened_url_input_option").value = "";
-    document.getElementById("shortened_url_input_option").hidden = true;
   });
 
 document
@@ -17,12 +33,21 @@ document
   .addEventListener("click", async (e) => {
     e.preventDefault();
     let inputValue = document.getElementById("shortened_url_input").value;
+    if (inputValue === "") {
+      errorShortUrl.hidden = false;
+      errorShortUrl.innerText = "You must enter short url!";
+      return;
+    }
+    errorShortUrl.hidden = true;
     await GetSpecificUrlStatistic(inputValue);
-    document.getElementById("url_input").value = "";
   });
 
 function showInput() {
   document.getElementById("shortened_url_input_option").hidden = false;
+}
+
+function hideStatistic() {
+  statisticDiv.hidden = true;
 }
 
 async function postNewUrl(url, inputCustomShortUrlValue) {
@@ -35,13 +60,45 @@ async function postNewUrl(url, inputCustomShortUrlValue) {
         customShortUrl: inputCustomShortUrlValue,
       },
     });
+    document.getElementById("shortened_url_input_option").value = "";
+    document.getElementById("shortened_url_input_option").hidden = true;
     if (res.data.length) {
-      console.log(res.data[0]);
+      newShortUrlP.hidden = false;
+      shortUrlAnchor.innerText = `http://localhost:3000/api/shorturl/${res.data[0]["shorturl-id"]}`;
+      shortUrlAnchor.setAttribute(
+        "href",
+        `http://localhost:3000/api/shorturl/${res.data[0]["shorturl-id"]}`
+      );
       return;
     }
-    console.log(res.data);
+    newShortUrlP.hidden = false;
+    shortUrlAnchor.innerText = `http://localhost:3000/api/shorturl/${res.data["shorturl-id"]}`;
+    shortUrlAnchor.setAttribute(
+      "href",
+      `http://localhost:3000/api/shorturl/${res.data["shorturl-id"]}`
+    );
   } catch (error) {
-    console.log(error.response.data);
+    switch (error.response.data) {
+      case "Invalid Url!":
+        errorUrl.hidden = false;
+        errorUrl.innerText = "Invalid Url!";
+        break;
+
+      case "This url does not exist!":
+        errorUrl.hidden = false;
+        errorUrl.innerText = "This url does not exist!";
+        break;
+
+      case "Invalid shortened url format!":
+        errorNewShortUrl.hidden = false;
+        errorNewShortUrl.innerText = "Invalid shortened url format!";
+        break;
+
+      case "Short url is already in use!":
+        errorNewShortUrl.hidden = false;
+        errorNewShortUrl.innerText = "Short url is already in use!";
+        break;
+    }
   }
 }
 
@@ -51,8 +108,22 @@ async function GetSpecificUrlStatistic(url) {
       method: "GET",
       url: `http://localhost:3000/api/statistic/${url}`,
     });
-    console.log(res.data);
+    errorShortUrl.hidden = true;
+    statisticDiv.hidden = false;
+    creationTimeP.innerText = `Creation Date: ${res.data.creationDate}`;
+    originalUrlP.innerText = `Original Url: ${res.data.originalUrl}`;
+    redirectCountP.innerText = `Redirect Count: ${res.data.redirectCount}`;
+    shorturlIdP.innerText = `Short Url Id: ${res.data["shorturl-id"]}`;
   } catch (error) {
-    console.log(error.response.data);
+    errorShortUrl.hidden = false;
+    switch (error.response.data) {
+      case "Invalid shortened url format!":
+        errorShortUrl.innerText = "Invalid shortened url format!";
+        break;
+
+      case "There is no such short url!":
+        errorShortUrl.innerText = "There is no such short url!";
+        break;
+    }
   }
 }
